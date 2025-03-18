b83108ab4500081c69488d665469faac4aa51afc
MariusCio <MariusCio@noreply.users.github.com>
2025-03-19 01:21:55 +0200

    modificari pt tema1

--- main.cpp
+++ main.cpp
@@ -1,124 +1,218 @@
 #include <iostream>
-#include <array>
-#include <chrono>
-#include <thread>
+#include <utility>
+#include <vector>
+#include <cstdlib>
+#include <random>
+#include <algorithm>
 
-#include <SFML/Graphics.hpp>
+class Pachet_carti {
+    std::vector<std::string>carti;
 
-#include <Helper.h>
-
-//////////////////////////////////////////////////////////////////////
-/// This class is used to test that the memory leak checks work as expected even when using a GUI
-class SomeClass {
 public:
-    explicit SomeClass(int) {}
+    Pachet_carti() :
+        carti{"A", "A", "A", "A", "A", "A",
+            "K", "K", "K", "K", "K", "K",
+            "Q", "Q", "Q", "Q", "Q", "Q",
+            "JOKER","JOKER"} {};
+
+
+    Pachet_carti(const Pachet_carti &x) {
+        carti = x.carti;
+    }
+
+    ~Pachet_carti() = default;
+
+    Pachet_carti& operator=(const Pachet_carti &x) {
+        carti = x.carti;
+        return *this;
+    }
+
+    void Amesteca_pachet() {
+        std::random_device rd;
+        std::mt19937 g(rd());
+        std::shuffle(carti.begin(), carti.end(), g);
+    }
+
+    std::vector<std::string> Extrage_mana() {
+        std::vector<std::string> mana;
+        for(int i=0;i<5;i++) {
+            mana.push_back(carti.back());
+            carti.pop_back();
+        }
+        return mana;
+    }
+
+    static void Afis_Mana(const std::vector<std::string>& mana) {
+        std::cout<<"Mana: ";
+        for(int i=0;i<mana.size();i++) {
+            std::cout<<mana[i]<<" ";
+        }
+    }
+
 };
 
-SomeClass *getC() {
-    return new SomeClass{2};
-}
-//////////////////////////////////////////////////////////////////////
 
+
+class Player {
+    std::string nume;
+    std::vector<std::string> mana;
+
+public:
+    Player(const std::string& nume,Pachet_carti& mana) {
+        this->nume=nume;
+        this->mana=mana.Extrage_mana();
+    }
+
+    Player &operator=(const Player &x) {
+        this->nume=x.nume;
+        this->mana=x.mana;
+        return *this;
+    }
+
+    ~Player() = default;
+
+    void Afis_Mana() {
+        std::cout<<nume<<": ";
+        for (size_t i = 0; i < mana.size(); i++) {
+            std::cout << i + 1 << ")" << mana[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    void Index_pt_carti(int nr, std::vector<int>& indecsi) {
+        int index;
+        for (int i=0;i<nr;i++) {
+            std::cin>>index;
+
+            while (index < 1 || index>mana.size()) {
+                std::cout<<"vezi cate carti ai in mana + numerotarea cartilor incepe de la 1"<<std::endl;
+                std::cin>>index;
+            }
+
+            indecsi.push_back(index - 1);
+        }
+    }
+
+    void Alege_carti() {
+        std::vector<int> indecsi;
+        int nr_carti;
+
+        std::cout<<nume<<", alege un numar de carti intre 1 si 3 : ";
+        std::cin>>nr_carti;
+
+        while (nr_carti > mana.size()){
+            std::cout<<"Mai ai doar "<<mana.size()<<" carti in mana"<<std::endl;
+            std::cin>>nr_carti;
+        }
+
+        while (nr_carti <1 || nr_carti >3) {
+            std::cout<<"Ai voie sa selectezi minim o carte si maxim 3"<<std::endl;
+            std::cin>>nr_carti;
+        }
+
+        if (nr_carti==1){ std::cout<<"indexul cartii este: "<<std::endl;
+            Index_pt_carti(nr_carti,indecsi);
+        }
+        else {std::cout<<"indexul celor "<<nr_carti<<" carti este: "<<std::endl;
+            Index_pt_carti(nr_carti,indecsi);
+        }
+
+        Mana_ramasa(indecsi);
+    }
+
+    void Mana_ramasa(std::vector<int> indecsi) {
+        std::sort(indecsi.rbegin(), indecsi.rend());
+
+        for (int idx : indecsi) {
+            mana.erase(mana.begin() + idx);
+        }
+        std::cout<<std::endl;
+
+        std::cout<<nume<<": ";
+        for (int i=0;i<mana.size();i++) {
+            std::cout<<i + 1<<")"<<mana[i]<< " ";
+        }
+        std::cout<<std::endl;
+    }
+
+
+};
+
+class Joc {
+    std::string nr_playeri;
+
+public:
+
+    // int Alege_nr_playeri() {
+    //     std::cin>>nr_playeri;
+    //
+    // }
+
+    Joc() = default;
+
+    explicit Joc(std::string nr_playeri)
+        : nr_playeri(std::move(nr_playeri)) {
+    }
+
+    int Alege_masa() {
+        return rand() % 3 + 1;
+    }
+};
+
+class Table{
+    std::string table_name;
+
+
+public:
+
+    void setTableName(Joc& joc) {
+        if (joc.Alege_masa()==1) this->table_name="ACE'S TABLE";
+        if (joc.Alege_masa()==2) this->table_name="KING'S TABLE";
+        if (joc.Alege_masa()==3) this->table_name="QUEEN'S TABLE";
+    }
+    void printTableName() const {
+        std::cout <<"Masa aleasa: " << table_name << std::endl;
+    }
+};
 
 int main() {
-    ///
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
-    }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
-    ///                Exemplu de utilizare cod generat                     ///
-    ///////////////////////////////////////////////////////////////////////////
-    Helper helper;
-    helper.help();
-    ///////////////////////////////////////////////////////////////////////////
+    Pachet_carti pachet;
+    pachet.Amesteca_pachet();
 
-    SomeClass *c = getC();
-    std::cout << c << "\n";
-    delete c;  // comentarea acestui rând ar trebui să ducă la semnalarea unui mem leak
+    Player player1("Stefi", pachet);
+    player1.Afis_Mana();
 
-    sf::RenderWindow window;
-    ///////////////////////////////////////////////////////////////////////////
-    /// NOTE: sync with env variable APP_WINDOW from .github/workflows/cmake.yml:31
-    window.create(sf::VideoMode({800, 700}), "My Window", sf::Style::Default);
-    ///////////////////////////////////////////////////////////////////////////
-    //
-    ///////////////////////////////////////////////////////////////////////////
-    /// NOTE: mandatory use one of vsync or FPS limit (not both)            ///
-    /// This is needed so we do not burn the GPU                            ///
-    window.setVerticalSyncEnabled(true);                                    ///
-    /// window.setFramerateLimit(60);                                       ///
-    ///////////////////////////////////////////////////////////////////////////
+    Player player2("Marus", pachet);
+    player2.Afis_Mana();
 
-    while(window.isOpen()) {
-        bool shouldExit = false;
-        sf::Event e{};
-        while(window.pollEvent(e)) {
-            switch(e.type) {
-            case sf::Event::Closed:
-                window.close();
-                break;
-            case sf::Event::Resized:
-                std::cout << "New width: " << window.getSize().x << '\n'
-                          << "New height: " << window.getSize().y << '\n';
-                break;
-            case sf::Event::KeyPressed:
-                std::cout << "Received key " << (e.key.code == sf::Keyboard::X ? "X" : "(other)") << "\n";
-                if(e.key.code == sf::Keyboard::Escape)
-                    shouldExit = true;
-                break;
-            default:
-                break;
-            }
-        }
-        if(shouldExit) {
-            window.close();
-            break;
-        }
-        using namespace std::chrono_literals;
-        std::this_thread::sleep_for(300ms);
+    std::cout << std::endl;
 
-        window.clear();
-        window.display();
-    }
-    return 0;
+    player1.Alege_carti();
+    std::cout << std::endl;
+    player2.Alege_carti();
+    std::cout << std::endl;
+    player1.Alege_carti();
 }
+
+
+
+
+    /*srand(time(0));
+    Joc joc;
+    Table table;
+    Player player1{"EU","Player1:"};
+    player1.afismana();
+    std::cout<<std::endl;
+    table.setTableName(joc);
+    table.printTableName();
+
+    Player player2{"AI","Player2:"};
+    player2.afismana();
+    std::cout<<std::endl;
+    table.setTableName(joc);
+    table.printTableName();
+
+    Pachet_carti pachet;
+    pachet.Amesteca_pachet();*/
+
+
