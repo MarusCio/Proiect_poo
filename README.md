3dd48acef0b128982863d714f1b8505e604638e3
MariusCio <MariusCio@noreply.users.github.com>
2025-03-19 23:44:44 +0200

    mai multe modificari

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-### Liar's Bar
+# Liar's Bar
 
 Ești în vizită prin Mama Rusia și ți se face sete, așa că intri în primul bar pe care îl vezi, ca să tragi și tu la măsea ceva bun.
 Te așezi frumos la masă și vezi cum vin trei rusnaci la tine. Ei te forțează să joci jocul lor, care este o combinație dintre Trombon și Ruleta rusească, iar dacă refuzi să te joci ești **cooked**.
 Rușii par să fie drăguți cu tine, așa că te lasă să alegi cu câți dintre ei te vei juca.
 
-## Instrucțiuni (fraierii citesc instrucțiuni, dar aici te joci cu viața ta...)
+### Instrucțiuni (**nimeni nu citește instrucțiuni, dar aici te joci cu viața ta...**)
 În funcție de masa aleasă random de unul dintre ruși la începutul fiecărei runde, trebuie să minți cât de bine poți sau să încerci să ghicești dacă unul dintre ruși a mințit.
 Fiecare dintre voi are cinci cărți din mulțimea {Q, K, A, JOKER}, precum și un revolver cu un glonț din șase.
 Pe scurt, dacă pierzi o rundă, ești cu un pas mai aproape de moarte. :) **Succes**!
--- main.cpp
+++ main.cpp
@@ -28,6 +28,10 @@ public:
     }
 
     void Amesteca_pachet() {
+        carti={"A", "A", "A", "A", "A", "A",
+            "K", "K", "K", "K", "K", "K",
+            "Q", "Q", "Q", "Q", "Q", "Q",
+            "JOKER","JOKER"};
         std::random_device rd;
         std::mt19937 g(rd());
         std::shuffle(carti.begin(), carti.end(), g);
@@ -44,7 +48,7 @@ public:
 
     static void Afis_Mana(const std::vector<std::string>& mana) {
         std::cout<<"Mana: ";
-        for(int i=0;i<mana.size();i++) {
+        for(size_t i=0;i<mana.size();i++) {
             std::cout<<mana[i]<<" ";
         }
     }
@@ -52,31 +56,37 @@ public:
 };
 
 
-
 class Player {
     std::string nume;
     std::vector<std::string> mana;
+    int viata;
+    std::vector<int> carti_alese;
+
 
 public:
-    Player(const std::string& nume,Pachet_carti& mana) {
+    Player(const std::string& nume,Pachet_carti& mana,int viata) {
         this->nume=nume;
         this->mana=mana.Extrage_mana();
+        this->viata=0;
+        this->carti_alese={};
     }
 
     Player &operator=(const Player &x) {
         this->nume=x.nume;
         this->mana=x.mana;
+        this->viata=x.viata;
+        this->carti_alese=x.carti_alese;
         return *this;
     }
 
     ~Player() = default;
 
-    void Afis_Mana() {
+    void Afis_Mana() const {
         std::cout<<nume<<": ";
         for (size_t i = 0; i < mana.size(); i++) {
-            std::cout << i + 1 << ")" << mana[i] << " ";
+            std::cout<<i+1<<")"<<mana[i]<<" ";
         }
-        std::cout << std::endl;
+        std::cout<<std::endl;
     }
 
     void Index_pt_carti(int nr, std::vector<int>& indecsi) {
@@ -84,7 +94,7 @@ public:
         for (int i=0;i<nr;i++) {
             std::cin>>index;
 
-            while (index < 1 || index>mana.size()) {
+            while (index < 1 || index>static_cast<int>(mana.size())) {
                 std::cout<<"vezi cate carti ai in mana + numerotarea cartilor incepe de la 1"<<std::endl;
                 std::cin>>index;
             }
@@ -93,31 +103,45 @@ public:
         }
     }
 
-    void Alege_carti() {
+    bool Alege_carti() {
         std::vector<int> indecsi;
         int nr_carti;
 
-        std::cout<<nume<<", alege un numar de carti intre 1 si 3 : ";
+        if (mana.empty()) {
+            return false;
+        }
+
+        std::cout<<nume<<", alege un numar de carti intre 1 si min(3,numarul tau de carți din mana) : ";
         std::cin>>nr_carti;
 
-        while (nr_carti > mana.size()){
+        while (nr_carti > static_cast<int>(mana.size())){
             std::cout<<"Mai ai doar "<<mana.size()<<" carti in mana"<<std::endl;
             std::cin>>nr_carti;
         }
 
         while (nr_carti <1 || nr_carti >3) {
-            std::cout<<"Ai voie sa selectezi minim o carte si maxim 3"<<std::endl;
+            std::cout<<"Ai voie sa selectezi minim o carte si maxim 3, respectiv numarul tau de carti din mana"<<std::endl;
             std::cin>>nr_carti;
         }
 
         if (nr_carti==1){ std::cout<<"indexul cartii este: "<<std::endl;
             Index_pt_carti(nr_carti,indecsi);
         }
-        else {std::cout<<"indexul celor "<<nr_carti<<" carti este: "<<std::endl;
+        else {std::cout<<"indexul celor "<<nr_carti<<" carti este: ";
             Index_pt_carti(nr_carti,indecsi);
         }
 
+        carti_alese.clear();
+        for (const int idx : indecsi) {
+            if (mana[idx]=="A") carti_alese.push_back(1);
+            else if (mana[idx]=="K") carti_alese.push_back(2);
+            else if (mana[idx]=="Q") carti_alese.push_back(3);
+            else carti_alese.push_back(4);
+        }
+
         Mana_ramasa(indecsi);
+
+        return !mana.empty();
     }
 
     void Mana_ramasa(std::vector<int> indecsi) {
@@ -126,93 +150,156 @@ public:
         for (int idx : indecsi) {
             mana.erase(mana.begin() + idx);
         }
-        std::cout<<std::endl;
 
         std::cout<<nume<<": ";
-        for (int i=0;i<mana.size();i++) {
+        for (size_t i=0;i<mana.size();i++) {
             std::cout<<i + 1<<")"<<mana[i]<< " ";
         }
         std::cout<<std::endl;
     }
 
+    std::vector<int> getCartiAlese() const {
+        return carti_alese;
+    }
+
+    bool Fara_carti() const {
+        return mana.empty();
+    }
+
+    void reset_carti(Pachet_carti& pachet) {
+        mana = pachet.Extrage_mana();
+        Afis_Mana();
+    }
+
+    void CresteViata() {
+        viata++;
+        if (viata>1) std::cout<<nume<<" acum are "<<viata<<" vieti"<<std::endl;
+        else std::cout<<nume<<" acum are "<<viata<<" viata"<<std::endl;
+    }
+
+    int Get_viata() const {
+        return viata;
+    }
 
 };
+
+class Table{
+    std::string table_name;
+    int index_table{};
+
+public:
+    Table() = default;
+
+    Table& operator=(const Table& x){
+        table_name=x.table_name;
+        index_table=x.index_table;
+        return *this;
+        }
+
+    ~Table() = default;
+
+    void setTableName() {
+        int masa=rand()%3+1;
+        if (masa==1) {this->table_name="ACE'S TABLE", index_table=1;}
+        else if (masa==2) {this->table_name="KING'S TABLE",index_table=2;}
+        else {this->table_name="QUEEN'S TABLE", index_table=3;}
+    }
+
+    void Afis_TableName() const {
+        std::cout <<"Masa aleasa: " << table_name << std::endl;
+    }
+
+    int Table_index() const {
+        return index_table;
+    }
+
+};
+
 
 class Joc {
     std::string nr_playeri;
 
 public:
 
-    // int Alege_nr_playeri() {
-    //     std::cin>>nr_playeri;
-    //
-    // }
+    void Alege_nr_playeri() {
+        std::cin>>nr_playeri;
+    }
 
     Joc() = default;
+    ~Joc() = default;
 
     explicit Joc(std::string nr_playeri)
         : nr_playeri(std::move(nr_playeri)) {
     }
 
-    int Alege_masa() {
-        return rand() % 3 + 1;
+    bool Minte(Player& player, Player& adversar, const Table& table) {
+        bool minciuna;
+
+        std::cout<<"daca crezi ca minte, scrie 1, altfel scrie 0: ";
+        std::cin>>minciuna;
+
+        if (minciuna==1) {
+            std::vector<int> carti_jucator = player.getCartiAlese();
+            int masa_aleasa = table.Table_index();
+            bool toate_corecte = std::all_of(carti_jucator.begin(), carti_jucator.end(),
+                                             [masa_aleasa](int carte) { return carte == masa_aleasa || carte==4; });
+
+            if (!toate_corecte) {
+                player.CresteViata();
+                return true;
+            }
+            else {
+                std::cout<<"nu a mintit!"<<std::endl;
+                adversar.CresteViata();
+                return true;
+            }
+        }
+        return false;
     }
+
 };
 
-class Table{
-    std::string table_name;
-
-
-public:
-
-    void setTableName(Joc& joc) {
-        if (joc.Alege_masa()==1) this->table_name="ACE'S TABLE";
-        if (joc.Alege_masa()==2) this->table_name="KING'S TABLE";
-        if (joc.Alege_masa()==3) this->table_name="QUEEN'S TABLE";
-    }
-    void printTableName() const {
-        std::cout <<"Masa aleasa: " << table_name << std::endl;
-    }
-};
 
 int main() {
     Pachet_carti pachet;
+    Table table;
+    Joc joc;
     pachet.Amesteca_pachet();
+    table.setTableName();
+    table.Afis_TableName();
 
-    Player player1("Stefi", pachet);
+    Player player1("Stefi", pachet,0);
     player1.Afis_Mana();
 
-    Player player2("Marus", pachet);
+    Player player2("Marus", pachet,0);
     player2.Afis_Mana();
 
-    std::cout << std::endl;
+    while (player1.Get_viata()<6 && player2.Get_viata()<6) {
 
-    player1.Alege_carti();
-    std::cout << std::endl;
-    player2.Alege_carti();
-    std::cout << std::endl;
-    player1.Alege_carti();
+        while (!player1.Fara_carti() && !player2.Fara_carti()) {
+            if (!player1.Alege_carti()) break;
+            std::cout << std::endl;
+            if (joc.Minte(player1,player2, table)) break;
+
+            if (!player2.Alege_carti()) break;
+            std::cout << std::endl;
+            if (joc.Minte(player2,player1,table)) break;
+        }
+        if (player1.Get_viata() >= 6 || player2.Get_viata() >= 6) break;
+
+        std::cout << std::endl;
+        std::cout <<"runda s-a terminat"<<std::endl;
+        std::cout <<"se genereaza o masa noua"<< std::endl;
+
+        pachet.Amesteca_pachet();
+        table.setTableName();
+        table.Afis_TableName();
+        player1.reset_carti(pachet);
+        player2.reset_carti(pachet);
+        std::cout<<std::endl;
+    }
+    std::cout << "Gata joculetul" << std::endl;
 }
 
-
-
-
-    /*srand(time(0));
-    Joc joc;
-    Table table;
-    Player player1{"EU","Player1:"};
-    player1.afismana();
-    std::cout<<std::endl;
-    table.setTableName(joc);
-    table.printTableName();
-
-    Player player2{"AI","Player2:"};
-    player2.afismana();
-    std::cout<<std::endl;
-    table.setTableName(joc);
-    table.printTableName();
-
-    Pachet_carti pachet;
-    pachet.Amesteca_pachet();*/
 
 
